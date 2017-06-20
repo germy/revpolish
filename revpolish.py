d0f411793603e3d2b90a89012dac2db72593afa6
Jeremy Lee <germyjermy@yahoo.com>
2017-06-20 14:49:01 -0400

    Added readme and stack

--- revpolish.py
+++ revpolish.py
@@ -1,13 +1,12 @@
 class RPN(object):
   
   def __init__(self):
-    self.num1 = None
-    self.num2 = None
+    self.stack = []
     self.OPERATORS = ['+', '-', '/', '*']
 
-    
+   # function prints the help menu
   def h(self):
-    print "-------------------------------------------"
+    print "--------------------help-------------------"
     print "This is a reverse polish noation calculator"
     print "valid inputs are float numbers and operators"
     print "to quit type 'q' or Ctrl-D (OS dependent)"
@@ -23,69 +22,70 @@ class RPN(object):
     print "30.0"
     print "-------------------------------------------"
 
+  # function prints the initialization text
   def intro(self):
     print "Welcome to Jeremy's reverse polish calculator"
     print "Type h for help"
 
-  def q(self):
-    print "RPN calculator has quit"
-
+  # function parses a single lined statement and handles any parsing errors
   def single(self, var):
     arr = var.split(" ")
     summ = None
     try:
       if len(arr) == 2:
-        if self.num1 == None:
+        if len(self.stack) == 0:
           summ =  eval(str(0) + " " + arr[1] + " " + arr[0])
         else:
-          summ =  eval(str(self.num1) + " " + arr[1] + " " + arr[0])        
+          summ =  eval(str(self.stack.pop()) + " " + arr[1] + " " + arr[0])        
       elif len(arr) == 3:
         summ =  eval(arr[0] + " " + arr[2] + " " + arr[1])
       if isinstance(summ, float) or isinstance(summ, int):
         print float(summ)
         return float(summ)
+      else:
+        raise
     except:
-      print "There was an issue pasing the input, the calculator will reset"
+      print "Error: there was an issue parsing the input"
+      print "the calculator state hasn't changed"
       return None
 
+  # function takes in input values and determines what actions accompany
+  # those particular actions
   def calc(self, invar):
     try:
       if invar == "h":
         self.h()
       elif invar == "q":
+        print "RPN calculator has quit"
         return False
       elif " " in invar:
-        self.num1 = self.single(invar)
-        self.num2 = None
+        self.stack.append(self.single(invar))
+      # checks for an operator input
       elif invar in self.OPERATORS:
-        if isinstance(self.num1, float) and isinstance(self.num2, float):
-          self.num1 = eval(str(self.num1) + " " + invar + " " + str(self.num2))
-          self.num2 = None
-          print self.num1
+        if len(self.stack) > 1:
+          # removes items off the stack and evaluates them with given operator
+          end = self.stack.pop()
+          onein = self.stack.pop()
+          self.stack.append(eval(str(onein) + " " + invar + " " + str(end)))
+          print self.stack[-1]
         else:
-          print "both numbers are not set, operator has not been applied"
+          print "Error: the stack doesn't contain enough items to apply"
+          print "an operator onto"
       else:
-        if self.num1 == None:
-          self.num1 = float(invar)
-          print self.num1
-        elif self.num2 == None:
-          self.num2 = float(invar)
-          print self.num2
-        else:
-          print "an unknown error occurred"
-          print "you may have entereed too many numbers in sequence"
-          print "the calculator will reset"
-          raise
+        # simply adds numbers to the stack, if it can't be interpreted then
+        # it raises an exception caught by the try except
+        self.stack.append(float(invar))
+        print self.stack[-1]
     except:
-      self.num1 = None
-      self.num2 = None
-      print "your input was invalid, the calculator has been cleared"
-
+      print "Error: your input was invalid"
+      print "the calculator state hasn't changed"
+      
+  # main function controls the general workflow    
   def main(self):
     self.intro()
     while True:
       invar = raw_input("> ")
-      if not self.calc(invar):
+      if self.calc(invar) == False:
         break
   
 
--- test.py
+++ test.py
@@ -2,17 +2,149 @@ from revpolish import RPN  # The module which contains the call to input
 import sys
 from cStringIO import StringIO
 
+# README
+# the calctester function captures the stdout for python to determin
+# what was printed after the calculator was run
 
-def test1():
 
+def calctester(expected, eq, instance):
   old_stdout = sys.stdout
   redirected_output = sys.stdout = StringIO()
-  rr = RPN()
-  rr.calc("5 6 *")
+  instance.calc(eq)
   sys.stdout = old_stdout
+  value = redirected_output.getvalue().strip()
+  if expected == value:
+    print "+ passed: " + str(eq) + " = " + str(value)
+  elif (("Error" in value) or ("help" in value) or ("quit" in value)) \
+    and expected in value:
+    print "+ passed: expected output occured"
+  else:
+    print "- failed: " + str(eq) + " = " + str(value)
 
-  assert '30.0' == redirected_output.getvalue().strip()
-  print "test finished"
+def test1():
+  rr = RPN()
+  calctester('30.0', '5 6 *', rr)
+  calctester('36.0', '6 +', rr)
+  calctester('34.0', '2 -', rr)
+  calctester('17.0', '2 /', rr)
+  calctester('6.0', '6', rr)
+  calctester('23.0', '+', rr)
+  calctester('10.6', '10.6', rr)
+  calctester('33.6', '+', rr)
+  calctester('3.36', '10 /', rr)
+  calctester('2.0', '2', rr)
+  calctester('6.72', '*', rr)
+  calctester('6.78', '6.78', rr)
+  calctester('4.0', '4', rr)
+  calctester('5.0', '5.0', rr)
+  calctester('9.0', '+', rr)
+  calctester('15.78', '+', rr)
+  print "test1 finished"
+  print ""
+  print "--------------"
+  print ""
+  
+def test2():
+  rr = RPN()
+  calctester('parsing', '5 6 * 6', rr)
+  calctester('operator', '*', rr)
+  calctester('30.0', '5 6 *', rr)
+  calctester('5.0', '5.0', rr)
+  calctester('5.0', '5.0', rr)
+  calctester('25.0', '*', rr)
+  calctester('30.0', '5 +', rr)
+  print "test2 finished"
+  print ""
+  print "--------------"
+  print ""
+  
+def test3():
+  rr = RPN()
+  calctester('5.0', '5 +', rr)
+  print "test3 finished"
+  print ""
+  print "--------------"
+  print ""
+  
+def test4():
+  rr = RPN()
+  calctester('-5.0', '5 -', rr)
+  print "test4 finished"
+  print ""
+  print "--------------"
+  print ""  
+  
+def test5():
+  rr = RPN()
+  calctester('13.0', '5 8 +', rr)
+  calctester('0.0', '13 -', rr)
+  print "test5 finished"
+  print ""
+  print "--------------"
+  print ""    
+
+def test6():
+  rr = RPN()
+  calctester('5.0', '5', rr)
+  calctester('9.0', '9', rr)
+  calctester('1.0', '1', rr)
+  calctester('8.0', '-', rr)
+  calctester('0.625', '/', rr)
+  print "test6 finished"
+  print ""
+  print "--------------"
+  print ""  
+  
+def test7():
+  rr = RPN()
+  calctester('5.0', '5', rr)
+  calctester('8.0', '8', rr)
+  calctester('13.0', '+', rr)
+  print "test7 finished"
+  print ""
+  print "--------------"
+  print ""    
+  
+def test8():
+  rr = RPN()
+  calctester('13.0', '5 8 +', rr)
+  calctester('0.0', '13 -', rr)
+  print "test8 finished"
+  print ""
+  print "--------------"
+  print ""     
+  
+def test9():
+  rr = RPN()
+  calctester('-3.0', '-3', rr)
+  calctester('-2.0', '-2', rr)
+  calctester('6.0', '*', rr)
+  calctester('5.0', '5', rr)
+  calctester('11.0', '+', rr)
+  print "test9 finished"
+  print ""
+  print "--------------"
+  print ""    
+    
+def test10():
+  rr = RPN()
+  calctester('invalid', 'geagae-3', rr)
+  calctester('help', 'h', rr)
+  calctester('quit', 'q', rr)
+  print "test10 finished"
+  print ""
+  print "--------------"
+  print ""    
+      
   
 if __name__ == "__main__":
   test1()
+  test2()
+  test3()
+  test4()
+  test5()
+  test6()
+  test7()
+  test8()
+  test9()
+  test10()
